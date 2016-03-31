ee57e1107ba6230ff12cc3b69f01227428ebfdd8
Hao Song <song860918@gmail.com>
2016-03-31 00:22:31 -0700

    Optimize tests for MatrixZero.

--- tst/ds/array/MatrixZeroTest.java
+++ tst/ds/array/MatrixZeroTest.java
@@ -1,5 +1,6 @@
 package ds.array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MatrixZeroTest {
@@ -8,23 +9,81 @@ public class MatrixZeroTest {
 
     @Test
     public void oneZeroTest() {
-        Matrix<Integer> matrix = matrixWithOneZero();
+        Matrix<Integer> matrix = matrixWithZeros(5, 5);
 
         matrixZero.parseMatrix(matrix.getMatrix());
 
         System.out.println(matrix);
+
+        verifyZeros(matrix.getMatrix(), 5, 5);
     }
 
-    private Matrix<Integer> matrixWithOneZero() {
+    @Test
+    public void noZeroTest() {
+        Matrix<Integer> matrix = matrixWithNoZero();
+
+        matrixZero.parseMatrix(matrix.getMatrix());
+
+        verifyZeros(matrix.getMatrix());
+    }
+
+    @Test
+    public void twoZeroTest() {
+        Matrix<Integer> matrix = matrixWithZeros(1, 2, 3, 4);
+
+        matrixZero.parseMatrix(matrix.getMatrix());
+        System.out.println(matrix);
+        verifyZeros(matrix.getMatrix(), 1, 2, 3, 4);
+    }
+
+    // First is row, second is column, and so on...
+    private void verifyZeros(Integer[][] matrix, int... index) {
+
+        boolean[] rows = new boolean[matrix.length];
+        boolean[] cols = new boolean[matrix[0].length];
+
+        for (int i = 0; i < index.length - 1; i += 2) {
+            rows[index[i]] = true;
+            cols[index[i + 1]] = true;
+        }
+
+        for (int i = 0; i < matrix.length; i += 2) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (rows[i] || cols[j]) {
+                    Assert.assertEquals(0, matrix[i][j].intValue());
+                } else {
+                    Assert.assertEquals(1, matrix[i][j].intValue());
+                }
+            }
+        }
+    }
+
+    private Matrix<Integer> matrixWithNoZero() {
 
         Integer[][] matrix = new Integer[10][10];
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if (i != 5 || j != 5) {
-                    matrix[i][j] = 1;
-                } else {
-                    matrix[i][j] = 0;
+                matrix[i][j] = 1;
+
+            }
+        }
+
+        return new Matrix<>(matrix);
+    }
+
+    private Matrix<Integer> matrixWithZeros(int... index) {
+
+        Integer[][] matrix = new Integer[10][10];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 1;
+                for (int k = 0; k < index.length - 1; k += 2) {
+                    if (index[k] == i && index[k + 1] == j) {
+                        matrix[i][j] = 0;
+                        break; // This should go to the outer for loop
+                    }
                 }
             }
         }
